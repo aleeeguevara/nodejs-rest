c24684db706c951ec0db3bfb43b524695fbbe09b
Alexandra <bguevaraale@gmail.com>
2022-02-23 12:36:03 -0300

    feat: add get to API

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,14 @@
 const Atendimento = require('../models/atendimentos');
 module.exports = app => {
-    app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimento e está dando um GET'));
+    app.get('/atendimentos', (req,res) => {
+        Atendimento.lista(res);
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+       
+        Atendimento.buscaporId(id, res);       
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
