b7cbc42eff587b576d6dfe16bbbcc564e8fb4eff
Alexandra <bguevaraale@gmail.com>
2022-02-23 11:09:54 -0300

    feat: add MomentJs to format dates & add validation to data before sending it to the data base

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,17 +1,50 @@
+const res = require('express/lib/response');
+const moment = require('moment'); //biblioteca formata data
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
     
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data= moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        
+        //validations
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao); //retorna true or false
+        const clienteEhValido= atendimento.cliente.length >=5; //retorna true or false
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro){
-                console.log(erro)
-            }else{
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ];
+        
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros= erros.length;
+
+        if(existemErros){
+           res.status(400).json(erros) 
+        }else{
+
+            
+            
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+            
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro);
+                }else{
+                    res.status(201).json(resultados);
+                }
+            })
+        }
     }
 
 }
